b4d5e7bb1cb192498d41c1a37004ee3988a9226f
muhammad-sohaib <muhammadsohaib.19477@gmail.com>
2025-12-09 19:21:19 +0500

    final makings

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-const header = document.querySelector("header")
+const header = document.querySelector("header");
+const menu = document.querySelector("#menu-icon");
+const navbar = document.querySelector(".navbar");
 
-window.addEventListener("scroll", function) {
-    header.classList.toggle("sticky",window.scrolly > 60)
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky", window.scrollY > 60);
 });
+
+// Mobile menu toggle
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+};
