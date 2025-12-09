6dac8d552e8717193b4cf5554a29bcb925f0a45e
muhammad-sohaib <muhammadsohaib.19477@gmail.com>
2025-12-09 19:57:02 +0500

    Responsivness Done

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
 };
+
+// WhatsApp Functionality for Contact Form
+function sendToWhatsapp() {
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+
+    let url = "https://wa.me/923121963155?text=" 
+        + "Name: " + encodeURIComponent(name) + "%0a"
+        + "Email: " + encodeURIComponent(email) + "%0a"
+        + "Message: " + encodeURIComponent(message);
+
+    window.open(url, '_blank').focus();
+}
